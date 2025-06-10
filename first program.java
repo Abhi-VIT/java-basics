bf06c94c4387f5a231a15e4b672c1adbb3c3d856
Abhishek kumar <abhishekkumar10042001@gmail.com>
2025-06-10 15:00:02 +0530

    Add files via upload

---
+++ first program.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[] args){
+        System.out.println("Welcome Abhishek Kumar");
+    }
+}
+//input --- System.in
+//output ---System.out
+// error --- System.err
+
+//javac FileName
+//java Class(with main)
